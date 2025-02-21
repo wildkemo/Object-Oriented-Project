40d7e06e8c3475cf669a86767b68bbf6965486e3
wildkemo <karimelbanna383@gmail.com>
2025-02-22 00:13:44 +0200

    added the rest of the pages design exept the contacts page

---
+++ home.js
@@ -0,0 +1,18 @@
+"use strict";
+// window.location.href = "index.html";
+
+function goToServices() {
+  window.location.href = "services.html";
+}
+
+function goToHome(){
+  window.location.href = "index.html";
+}
+
+function goToProducs(){
+  window.location.href = "products.html";
+}
+
+function goToAbout(){
+  window.location.href = "about.html";
+}
